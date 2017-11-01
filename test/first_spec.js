4fd177e727db507b84fbe13b7d65393b152ba8b7
Ernesto Rubio Fuentes <neto.r27@gmail.com>
2017-11-01 12:04:09 -0600

    Adding another test with coffee script

--- test/first_spec.js
+++ test/first_spec.js
@@ -1,8 +1,8 @@
 var assert = require("assert");
 
-describe("A feature", function(){
-    describe("A scenario", function(){
-        it("does something expected", function(){
+describe("A feature", function () {
+    describe("A scenario", function () {
+        it("does something expected", function () {
             assert(true);
         });
     });
